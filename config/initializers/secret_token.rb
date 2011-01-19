a56b383375ca4b837bbad5ace4bb0a50bde4bc93
lindyhan <lindyhan@gmail.com>
2011-01-19 20:58:36 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Areyounuts::Application.config.secret_token = '09075a1193ecf3e9f653a9a52c36fed09f1f651aef92b8457200b73b5e45217aa6d6851a88fde2c4015b1560b79e650a707357809c2b29752707ef5d62494f26'
